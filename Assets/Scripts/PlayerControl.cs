135f12c50add726c9f169141ba402c14ddbec008
jacobv1234 <jacobjv1234@gmail.com>
2026-02-18 10:30:36 +0000

    initial car physics

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,214 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField]
+    private float turnSpeed = 200.0f;
+    [SerializeField]
+    private float rotationAccel = 10.0f;
+    [SerializeField]
+    private float rotationFriction = 50.0f;
+    [SerializeField]
+    private float topSpeed = 20.0f;
+    [SerializeField]
+    private float acceleration = 0.3f;
+    [SerializeField]
+    private float groundFriction = 1.0f;
+    [SerializeField]
+    private float airFriction = 0.3f;
+
+    private float objectHeight = 1.5f;
+
+    [SerializeField]
+    private float speed = 0.0f;
+    [SerializeField]
+    private float rotation = 0.0f;
+    [SerializeField]
+    private bool grounded = false;
+
+    bool UpPressed, DownPressed, LeftPressed, RightPressed;
+
+    private InputSystem_Actions actions;
+
+    private Vector3 respawnCoords;
+    private Quaternion respawnRotation;
+    
+
+    void Accelerate()
+    {
+        if (!grounded)
+        {
+            return;
+        }
+        if (speed < topSpeed)
+        {
+            speed += acceleration;
+        }
+        else
+        {
+            speed = topSpeed;
+        }
+    }
+
+    void Decelerate()
+    {
+        if (!grounded)
+        {
+            return;
+        }
+        if (speed > -topSpeed)
+        {
+            speed -= acceleration;
+        }
+        else
+        {
+            speed = -topSpeed;
+        }
+    }
+
+    void SteerRight()
+    {
+        if (!grounded)
+        {
+            return;
+        }
+        if (rotation < turnSpeed)
+        {
+            rotation += rotationAccel;
+        }
+        else
+        {
+            rotation = turnSpeed;
+        }
+    }
+
+    void SteerLeft()
+    {
+        if (!grounded)
+        {
+            return;
+        }
+        if (rotation > -turnSpeed)
+        {
+            rotation -= rotationAccel;
+        }
+        else
+        {
+            rotation = -turnSpeed;
+        }
+    }
+
+    void Move()
+    {
+        Vector3 moveVector = new Vector3(0, 0, speed);
+
+        transform.Translate(moveVector * Time.deltaTime);
+    }
+
+    void Turn()
+    {
+        Vector3 rotationVector = new Vector3(0, rotation, 0);
+        transform.Rotate(rotationVector * Time.deltaTime);
+    }
+
+    void Respawn()
+    {
+        transform.position = respawnCoords;
+        transform.rotation = respawnRotation;
+    }
+
+    void CheckAirborne()
+    {
+        Vector3 offset = new Vector3(0,1,0);
+        grounded = Physics.Raycast(transform.position + offset, Vector3.down, objectHeight);
+    }
+
+    void DecayValues()
+    {
+        float friction;
+        if (grounded)
+        {
+            friction = groundFriction;
+        }
+        else
+        {
+            friction = airFriction;
+        }
+
+        if ((!UpPressed && !DownPressed) || !grounded) // airborne or not controlling
+        {
+            if (speed < 0.0f)
+            {
+                speed += topSpeed * friction * Time.deltaTime;
+                if (speed > 0.0f)
+                {
+                    speed = 0.0f;
+                }
+            }
+            else
+            {
+                speed -= topSpeed * friction * Time.deltaTime;
+                if (speed < 0.0f)
+                {
+                    speed = 0.0f;
+                }
+            }
+        }
+
+        if (!LeftPressed && !RightPressed)
+        {
+            if (rotation < 0.0f)
+            {
+                rotation += turnSpeed * rotationFriction * Time.deltaTime;
+                if (rotation > 0.0f)
+                {
+                    rotation = 0.0f;
+                }
+            }
+            else
+            {
+                rotation -= turnSpeed * rotationFriction * Time.deltaTime;
+                if (rotation < 0.0f)
+                {
+                    rotation = 0.0f;
+                }
+            }
+        }
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        actions = new InputSystem_Actions();
+        actions.Enable();
+
+        respawnCoords = transform.position;
+        respawnRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // controls
+        UpPressed = actions.Player.Accelerate.IsPressed();
+        DownPressed = actions.Player.Brake.IsPressed();
+        LeftPressed = actions.Player.Left.IsPressed();
+        RightPressed = actions.Player.Right.IsPressed();
+
+        if (UpPressed) {Accelerate();}
+        if (DownPressed) {Decelerate();}
+        if (LeftPressed) {SteerLeft();}
+        if (RightPressed) {SteerRight();}
+
+        if (actions.Player.Respawn.WasPressedThisFrame()) {Respawn();}
+
+        // physics
+        Move();
+        Turn();
+        CheckAirborne();
+        DecayValues();
+    }
+}
